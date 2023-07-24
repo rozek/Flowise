76930d090a1c32f2f9678a350ddcb5f4227f10f0
Andreas Rozek <A.Rozek@gmx.de>
2023-07-24 15:46:12 +0200

    Update ChatLMStudio.ts

--- packages/components/nodes/chatmodels/ChatLMStudio/ChatLMStudio.ts
+++ packages/components/nodes/chatmodels/ChatLMStudio/ChatLMStudio.ts
@@ -1,7 +1,6 @@
 import { INode, INodeData, INodeParams } from '../../../src/Interface'
 import { getBaseClasses } from '../../../src/utils'
-import { OpenAIChat } from 'langchain/llms/openai'
-import { OpenAIChatInput } from 'langchain/chat_models/openai'
+import { ChatOpenAI, OpenAIChatInput } from 'langchain/chat_models/openai'
 
 class ChatLMStudio_ChatModels implements INode {
     label: string
@@ -20,7 +19,7 @@ class ChatLMStudio_ChatModels implements INode {
         this.icon  = 'lmstudio.png'
         this.category = 'Chat Models'
         this.description = 'Use LM Studio Server instead of OpenAI'
-        this.baseClasses = [this.type, 'BaseChatModel', ...getBaseClasses(OpenAIChat)]
+        this.baseClasses = [this.type, ...getBaseClasses(ChatOpenAI)]
         this.inputs = [
             {
                 label: 'Base Path',
@@ -46,17 +45,17 @@ class ChatLMStudio_ChatModels implements INode {
             {
                 label: 'Temperature',
                 name: 'temperature',
-                type: 'string',   // because Flowise has problems with DE locale
-                default: '0.9',
+                type: 'number',
+                default: 0,
                 optional: true,
                 additionalParams: true,
-                description: 'text generation "temperature" (0...1)'
+                description: 'text generation "temperature" (0...2)'
             },
             {
                 label: 'Max Tokens',
                 name: 'maxTokens',
-                type: 'string',   // because Flowise has problems with DE locale
-                default: '-1',
+                type: 'number',
+                default: -1,
                 optional: true,
                 additionalParams: true,
                 description: 'max. number of tokens to generate (or -1)'
@@ -64,8 +63,8 @@ class ChatLMStudio_ChatModels implements INode {
             {
                 label: 'Top Probability',
                 name: 'topP',
-                type: 'string',   // because Flowise has problems with DE locale
-                default:'0.95',
+                type: 'number',
+                default:0.1,
                 optional: true,
                 additionalParams: true,
                 description: 'top-p setting (0...1)'
@@ -73,20 +72,28 @@ class ChatLMStudio_ChatModels implements INode {
             {
                 label: 'Frequency Penalty',
                 name: 'frequencyPenalty',
-                type: 'string',   // because Flowise has problems with DE locale
-                default:'1.1',
+                type: 'number',
+                default:1.1,
                 optional: true,
                 additionalParams: true,
-                description: 'frequency penalty (0...2)'
+                description: 'frequency penalty (-2...2)'
             },
             {
                 label: 'Presence Penalty',
                 name: 'presencePenalty',
-                type: 'string',   // because Flowise has problems with DE locale
-                default:'1.1',
+                type: 'number',
+                default:0,
                 optional: true,
                 additionalParams: true,
-                description: 'presence penalty (0...2)'
+                description: 'presence penalty (-2...2)'
+            },
+            {
+                label: 'Stop Sequence',
+                name: 'stop',
+                type: 'string',
+                default: '### Instruction:',
+                additionalParams: true,
+                description: 'sequence at which to stop token generation'
             },
             {
                 label: 'Timeout',
@@ -108,22 +115,24 @@ class ChatLMStudio_ChatModels implements INode {
         const topP             = nodeData.inputs?.topP as string
         const frequencyPenalty = nodeData.inputs?.frequencyPenalty as string
         const presencePenalty  = nodeData.inputs?.presencePenalty as string
+        const stop             = nodeData.inputs?.stop as string
         const timeout          = nodeData.inputs?.timeout as string
 
-        const obj: Partial<OpenAIChatInput> = {
+        const obj: Partial<OpenAIChatInput> & { openAIApiKey?: string } = {
             temperature: parseFloat(temperature),
             modelName,
+            openAIApiKey:'sk-xxxx',                              // just a dummy
             streaming:(streaming == true)
         }
 
         if (maxTokens)        obj.maxTokens        = parseInt(maxTokens, 10)
-        if (topP)             obj.topP             = parseInt(topP, 10)
-        if (frequencyPenalty) obj.frequencyPenalty = parseInt(frequencyPenalty, 10)
-        if (presencePenalty)  obj.presencePenalty  = parseInt(presencePenalty, 10)
+        if (topP)             obj.topP             = parseFloat(topP)
+        if (frequencyPenalty) obj.frequencyPenalty = parseFloat(frequencyPenalty)
+        if (presencePenalty)  obj.presencePenalty  = parseFloat(presencePenalty)
+        if (stop)             obj.stop             = [stop]
         if (timeout)          obj.timeout          = parseInt(timeout, 10)
 
-        const model = new OpenAIChat(obj, { basePath })
-
+        const model = new ChatOpenAI(obj, { basePath })
         return model
     }
 }
